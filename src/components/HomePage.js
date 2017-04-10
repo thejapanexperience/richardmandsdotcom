354ebe61dd80449f0f23faba24f7f381d54cdb37
Richard Mands <richard@eduku.org>
2017-04-10 22:09:08 +0800

    update

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,7 +4,22 @@ import {Link} from 'react-router';
 const HomePage = () => {
   return (
     <div>
-      <span>hi</span>
+      <div className='mainContainer'>
+        <div className="section0">
+          <div className="navBox">
+            <div className="nav1"></div>
+            <div className="nav2"></div>
+            <div className="nav3"></div>
+            <div className="nav4"></div>
+            <div className="nav5"></div>
+            <div className="nav6"></div>
+          </div>
+        </div>
+        <div className="section1"></div>
+        <div className="section2"></div>
+        <div className="section3"></div>
+        <div className="section4"></div>
+      </div>
     </div>
   );
 };

c0b2e4ffcdc38893ba6fe9133fc53c655d1b6ae2
Richard Mands <richard@eduku.org>
2017-04-11 20:00:01 +0800

    update

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -6,6 +6,7 @@ const HomePage = () => {
     <div>
       <div className="backgroundImage"/>
       <div className='mainContainer'>
+
         <div className="section0">
           <div className="navBox">
             <div className="nav1">
@@ -16,26 +17,42 @@ const HomePage = () => {
             <div className="nav2" />
             <div className="nav3">
               <div className="nav3Text">
-                Coding
+                {/* Coding */}
               </div>
               <div className="nav3Text">
-                Working Samples
+                {/* My Work */}
               </div>
             </div>
             <div className="nav4">
               <div className="nav4Text">
-                Other Skills
+                {/* Skills */}
               </div>
               <div className="nav4Text">
-                Resume
+                {/* Resume */}
               </div>
             </div>
             <div className="nav5"></div>
             <div className="nav6" />
           </div>
         </div>
+
         <div className="section1Box">
-          <div className="section1"/>
+          <div className="section1Box">
+
+            <div className="section1">
+              <div className="flexImage">
+                <img className="section1BoxImage" src="../images/richard1Crop.jpg" alt="richardmands"/>
+              </div>
+              <div className="flexText">
+                <div className="section1Text1">
+                  Richard Mands
+                </div>
+                <div className="section1Text2">
+                  Full-Stack Javascript Developer
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="section2Box">
           <div className="section2" />

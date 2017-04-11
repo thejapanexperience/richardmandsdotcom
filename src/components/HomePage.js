404cc342672cbbab9f65897db5447bffb5703300
Richard Mands <richard@eduku.org>
2017-04-11 17:02:18 +0800

    update

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,21 +4,48 @@ import {Link} from 'react-router';
 const HomePage = () => {
   return (
     <div>
+      <div className="backgroundImage"/>
       <div className='mainContainer'>
         <div className="section0">
           <div className="navBox">
-            <div className="nav1"></div>
-            <div className="nav2"></div>
-            <div className="nav3"></div>
-            <div className="nav4"></div>
+            <div className="nav1">
+              <div className="nav1Text">
+                richardmands.com
+              </div>
+            </div>
+            <div className="nav2" />
+            <div className="nav3">
+              <div className="nav3Text">
+                Coding
+              </div>
+              <div className="nav3Text">
+                Working Samples
+              </div>
+            </div>
+            <div className="nav4">
+              <div className="nav4Text">
+                Other Skills
+              </div>
+              <div className="nav4Text">
+                Resume
+              </div>
+            </div>
             <div className="nav5"></div>
-            <div className="nav6"></div>
+            <div className="nav6" />
           </div>
         </div>
-        <div className="section1"></div>
-        <div className="section2"></div>
-        <div className="section3"></div>
-        <div className="section4"></div>
+        <div className="section1Box">
+          <div className="section1"/>
+        </div>
+        <div className="section2Box">
+          <div className="section2" />
+        </div>
+        <div className="section3Box">
+          <div className="section3" />
+        </div>
+        <div className="section4Box">
+          <div className="section4" />
+        </div>
       </div>
     </div>
   );

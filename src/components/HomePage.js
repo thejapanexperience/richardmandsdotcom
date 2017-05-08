861931882e5e595b8c83aebd373529f61bb0c9c1
Richard Mands <richard@eduku.org>
2017-05-08 21:09:44 +0800

    update

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -255,7 +255,7 @@ const HomePage = () => {
               Videography & Editing
             </div>
             <div className="section7Text">
-              Lorem oopsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              Lorem oopsum doloor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </div>
             <div className="portfolioYoutubeBox">
               <div className="portfolioYoutubeVideo">

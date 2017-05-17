e91fc3c16dfc67ac5b45d938533787ffc4fd0168
Richard Mands <richard@eduku.org>
2017-05-18 02:08:55 +0800

    update

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -252,7 +252,7 @@ export default class HomePage extends React.Component {
                 PHOTOGRAPHY
               </div>
               <div className="section7Text">
-                Lorem oopsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                I'm an experienced photographer and have shot a variety of paid jobs both on location and in the studio. 
               </div>
               <div className="section7ImageBox">
                 <img className="section7Image" src={require("../images/richardCollageWide02.jpg")} alt="richardmandsphotography"/>

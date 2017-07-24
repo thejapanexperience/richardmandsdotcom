0e18ab08e3d40750cbc641561f9b09cf751c280d
Richard Mands <richard@eduku.org>
2017-07-24 19:54:35 +0100

    react demo 1

--- tools/distServerExpress01.js
+++ tools/distServerExpress01.js
@@ -1,15 +1,14 @@
 import historyApiFallback from 'connect-history-api-fallback';
-import webpack from 'webpack';
-import webpackDevMiddleware from 'webpack-dev-middleware';
-import webpackHotMiddleware from 'webpack-hot-middleware';
 import {chalkSuccess} from './chalkConfig';
-import config from '../webpack.config.dev';
 import express from 'express';
 import http from 'http';
+import path from 'path';
 
-// SET SERVER PORT
-const PORT = process.env.PORT || 8001;
+const app = express();
+const server = http.createServer(app);
+const port = (process.env.PORT || 4000);
 const MONGODB_URI = process.env.MONGODB_URI || `mongodb://localhost/AllPurposeApp001`
+console.log(chalkSuccess('Starting Express dist server...'));
 
 // MONGOOSE
 const mongoose = require('mongoose')
@@ -18,36 +17,17 @@ mongoose.connect(MONGODB_URI, err => {
   console.log(err || `Mongo connected to ${MONGODB_URI}`);
 })
 
-const bundler = webpack(config);
-const app = express();
-const server = http.createServer(app);
-console.log(chalkSuccess('Starting Express server...'));
-
 // ROUTES
 app.use('/api', require('./routes/api'));
 
-app.use(express.static('src/*.html'));
+app.use(express.static(path.join(__dirname, '../dist')));
 app.use(historyApiFallback());
-app.use(webpackHotMiddleware(bundler));
-app.use(webpackDevMiddleware(bundler, {
-    // Dev middleware can't access config, so we provide publicPath
-    publicPath: config.output.publicPath,
 
-    // These settings suppress noisy webpack output so only errors are displayed to the console.
-    noInfo: false,
-    quiet: false,
-    stats: {
-      assets: false,
-      colors: true,
-      version: false,
-      hash: false,
-      timings: false,
-      chunks: false,
-      chunkModules: false
-    },
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
 
-    // for other settings see
-    // http://webpack.github.io/docs/webpack-dev-middleware.html
-}));
-server.listen(PORT);
+// app.use(express.static('dist/*.html'));
+
+server.listen(port);
 console.log(chalkSuccess('Express server is listening on port: ' + server.address().port));

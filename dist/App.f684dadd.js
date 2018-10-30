b6ffcb259fc49f3aab7cc9b68beab1c120dcd3d0
Richard Mands <richardmands@BGNEW161.local>
2018-10-30 20:25:01 +0000

    css fix for resume links

--- dist/App.f684dadd.js
+++ dist/App.f684dadd.js
@@ -41747,7 +41747,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56529" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57320" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
